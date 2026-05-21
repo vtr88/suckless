fb8e66083d35d3232f6b736a26e26ff87a69b037
Vitor <vtr88@yahoo.com.br>
2026-05-21 11:49:24 -0300

    Darken slock failure color

--- slock/config.def.h
+++ slock/config.def.h
@@ -5,7 +5,7 @@ static const char *group = "nogroup";
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#b8bb26",   /* during input */
-	[FAILED] = "#d65d0e",   /* wrong password */
+	[FAILED] = "#af3a03",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
